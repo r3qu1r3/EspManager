c5432a082479b2cf8b09324a36d1316a0b14ba6c
r3qu1r3 <98073814+r3qu1r3@users.noreply.github.com>
2025-10-10 16:14:21 -0700

    Update plugin.lua

--- Assets/plugin.lua
+++ Assets/plugin.lua
@@ -1,1 +1,26 @@
+type PluginData = {
+    Install: (object: any) -> ();
+    Update: (pool: any, target: any, boundingBox: any) -> ();
+    Style: any?; 
+    Settings: any?; 
 
+}
+
+local Plugin = { }; 
+Plugin.__index = Plugin;
+
+function Plugin.new(Name, Install, Initialize, Update, Settings, Style, OutlineStyle): PluginData
+    local newPlugin = setmetatable({
+        Name = Name;
+        Install = Install; 
+        Initialize = Initialize;
+        Update = Update; 
+        Style = Style;
+        OutlineStyle = OutlineStyle;
+        Settings = Settings;
+    }, Plugin);
+
+    return newPlugin;
+end; 
+
+return Plugin;
